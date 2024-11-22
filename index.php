e6cca058314dcfd0040fb5ef023908d753505c3d
vilasbtw <kaiquevilas@icloud.com>
2024-11-22 14:15:10 -0300

    Adicionar validação de sessão

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+if (isset($_SESSION['tipoUsuario'])) {
+    if ($_SESSION['tipoUsuario'] == 1) {
+        header("Location: admin.php");
+    } else {
+        header("Location: user.php");
+    }
+    exit(); 
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
