1bfc38c777f7bb58833804489128b820933efead
stledi <75648334+Gustavo-Stedile@users.noreply.github.com>
2024-12-02 10:28:30 -0300

    Delete cadastrarEspaco.php

--- cadastrarEspaco.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "espacos/cadastrarEspaco.php";
-
-$nome = $_POST["nome"];
-$endereco = $_POST["endereco"];
-
-
-cadastrarEspaco($nome, $endereco);
-header("Location: home/admHome.php"); 
