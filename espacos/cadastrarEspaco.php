89e009b07bab89f40532445e375d067559e04edf
stledi <75648334+Gustavo-Stedile@users.noreply.github.com>
2024-12-02 10:26:17 -0300

    Delete espacos directory

--- espacos/cadastrarEspaco.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include $_SERVER["DOCUMENT_ROOT"]."/naturezaviva/db/conexao.php";
-
-function cadastrarEspaco($nome, $endereco) {
-  $con = conectar();
-  mysqli_query($con, "INSERT INTO Espacos (nome, endereco) VALUES ('$nome', '$endereco')");
-}
