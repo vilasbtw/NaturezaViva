005190afaf7394e9a864b18a2e3b8697116883c8
stledi <75648334+Gustavo-Stedile@users.noreply.github.com>
2024-12-02 10:26:44 -0300

    Delete horarios directory

--- horarios/adicionarHorario.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include $_SERVER["DOCUMENT_ROOT"]."/naturezaviva/db/conexao.php";
-
-
-function inserirHorarioNoEspaco($espacoId, $inicio, $fim) {
-    $con = conectar();
-    mysqli_query($con, "INSERT INTO Horarios (inicio, fim, id_espaco) VALUES ('$inicio', '$fim', $espacoId);");
-}
