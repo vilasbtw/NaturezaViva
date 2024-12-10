af6934b87057efcb1408e52bda00b03a1b471d19
vilasbtw <kaiquevilas@icloud.com>
2024-12-10 11:26:36 -0300

    Aprimirorar ajuste de validacao de existencia de usuario

--- actions/cadastrar.php
+++ actions/cadastrar.php
@@ -8,17 +8,24 @@
     $conexao = conectar();
 
     $queryVerificaUsuario = "SELECT * FROM Usuarios WHERE nome = '$nome'";
-    $resultado = mysqli_query($conexao, $queryVerificaUsuario);
+    $resultadoUsuario = mysqli_query($conexao, $queryVerificaUsuario);
+    $linhasUsuario = mysqli_num_rows($resultadoUsuario);
 
-    if (mysqli_num_rows($resultado) > 0) {
+    $queryVerificaAdministrador = "SELECT * FROM Administradores WHERE nome = '$nome'";
+    $resultadoAdministrador = mysqli_query($conexao, $queryVerificaAdministrador);
+    $linhasAdministrador = mysqli_num_rows($resultadoAdministrador);
+
+    if ($linhasUsuario > 0 || $linhasAdministrador > 0) {
         echo "Usuário já existente. Escolha outro nome.";
     } else {
         $queryCadastro = "INSERT INTO Usuarios(nome, senha) VALUES ('$nome', '$senha')";
         $inseriu = mysqli_query($conexao, $queryCadastro);
 
         if ($inseriu) {
+            echo "<a href='../index.php'>voltar ao inicio</a><br>";
             echo "Usuário cadastrado com sucesso!";
         } else {
+            echo "<a href='../index.php'>voltar ao inicio</a><br>";
             echo "Erro ao cadastrar o usuário. Tente novamente.";
         }
     }
